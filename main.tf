a6df4dc25680f9f259f6bf749ac0464bb33fcb30
Brent Farand <36188155+BFarand@users.noreply.github.com>
2023-07-12 14:42:33 -0600

    fix: exclude windows/linux exclusive resources as needed

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+locals {
+  is_windows = var.operating_system == "WINDOWS"
+}
+
 resource "aws_ssm_patch_baseline" "baseline" {
   count = module.this.enabled ? 1 : 0
 
@@ -16,11 +20,15 @@ resource "aws_ssm_patch_baseline" "baseline" {
     content {
       approve_after_days  = approval_rule.value.approve_after_days
       compliance_level    = approval_rule.value.compliance_level
-      enable_non_security = approval_rule.value.enable_non_security
+      enable_non_security = local.is_windows ? false : approval_rule.value.enable_non_security
 
-      patch_filter {
-        key    = "PATCH_SET"
-        values = ["OS"]
+      dynamic "patch_filter" {
+        for_each = local.is_windows ? ["true"] : []
+
+        content {
+          key    = "PATCH_SET"
+          values = ["OS"]
+        }
       }
 
       # https://docs.aws.amazon.com/cli/latest/reference/ssm/describe-patch-properties.html
@@ -36,13 +44,14 @@ resource "aws_ssm_patch_baseline" "baseline" {
   }
 
   # Application Approval Rules
+  # Currently only applicable with a Windows OS.
   dynamic "approval_rule" {
-    for_each = var.patch_baseline_application_approval_rules
+    for_each = local.is_windows ? var.patch_baseline_application_approval_rules : {}
 
     content {
       approve_after_days  = approval_rule.value.approve_after_days
       compliance_level    = approval_rule.value.compliance_level
-      enable_non_security = approval_rule.value.enable_non_security
+      enable_non_security = false # Windows does not support this.
 
       patch_filter {
         key    = "PATCH_SET"
